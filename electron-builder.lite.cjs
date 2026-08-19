d3531749152f7bc6135248f87b3458a94ae8e7d5
Blank_Answer <97771966+blankanswer@users.noreply.github.com>
2026-08-19 16:56:44 +0800

    feat(lite): use new app icon across desktop packages

--- electron-builder.lite.cjs
+++ electron-builder.lite.cjs
@@ -47,15 +47,18 @@ module.exports = {
   }),
   mac: {
     ...(base.mac || {}),
+    icon: 'assets/lite-icon.svg',
     identity: null,
     target: [{ target: 'zip', arch: [currentArch] }],
   },
   win: {
     ...(base.win || {}),
+    icon: 'assets/lite-icon.svg',
     target: [{ target: 'nsis', arch: [currentArch] }],
   },
   linux: {
     ...(base.linux || {}),
+    icon: 'assets/lite-icon.svg',
     target: [{ target: 'AppImage', arch: [currentArch] }],
   },
 };
